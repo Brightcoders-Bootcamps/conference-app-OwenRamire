8e4db3495ed8fd3949afa66f57c54e2a6c18706d
OwenRamire <owenomar_ramirez@ucol.mx>
2020-07-21 14:20:09 -0500

    Create upcoming, completed and add screens

---
+++ conferenceOwen/src/screens/menuUser/AddScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, Text, StyleSheet} from 'react-native';
+
+export default function Completed() {
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <Text style={styles.title}>Add one</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+  },
+});
