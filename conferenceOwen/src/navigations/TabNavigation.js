5a7f60cdf472ace365d40c23a1e6b9bad3f431ee
OwenRamire <owenomar_ramirez@ucol.mx>
2020-07-24 18:25:10 -0500

    Create TopTabNavigator

--- conferenceOwen/src/navigations/TabNavigation.js
+++ conferenceOwen/src/navigations/TabNavigation.js
@@ -4,6 +4,7 @@ import Upcoming from '../screens/menuUser/UpcomingScreen';
 import Completed from '../screens/menuUser/CompletedScreen';
 import AddConference from '../screens/menuUser/AddScreen';
 import {Colors} from '../themes/Colors';
+import Icons from 'react-native-vector-icons/AntDesign';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -11,19 +12,56 @@ export default function Navegation() {
   return (
     <Tab.Navigator
       tabBarOptions={{
-        activeTintColor: Colors.activeTab,
-        inactiveTintColor: Colors.inactiveTab,
+        showIcon: true, //Propertie to show icons in the top tab bar
+        activeTintColor: Colors.activeTab, //Color of the selected button
+        inactiveTintColor: Colors.inactiveTab, // Color of the unselected button
         pressColor: Colors.pressColor,
         pressOpacity: Colors.pressColor,
+        // Style object for the tab bar
+        style: {
+          height: 80,
+        },
+        //This is the style of the thin line
         indicatorStyle: {
-          backgroundColor: Colors.indicatorTab,
+          backgroundColor: Colors.activeTab,
+        },
+        labelStyle: {
+          fontFamily: 'AlBayan',
+          marginVertical: 20,
+        },
+        iconStyle: {
+          position: 'absolute',
+          right: -10,
+          top: 10,
         },
       }}
       initialRouteName="Upcoming"
       tabBarPosition="top">
-      <Tab.Screen name="Upcoming" component={Upcoming} />
-      <Tab.Screen name="Completed" component={Completed} />
-      <Tab.Screen name="Add" component={AddConference} />
+      <Tab.Screen
+        name="Upcoming"
+        component={Upcoming}
+        options={{
+          tabBarLabel: 'Upcoming',
+        }}
+      />
+      <Tab.Screen
+        name="Completed"
+        component={Completed}
+        options={{
+          tabBarLabel: 'Completed',
+        }}
+      />
+      <Tab.Screen
+        name="Add"
+        component={AddConference}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({focused, color}) => {
+            focused = 'plus';
+            return <Icons name={focused} size={30} color={color} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
