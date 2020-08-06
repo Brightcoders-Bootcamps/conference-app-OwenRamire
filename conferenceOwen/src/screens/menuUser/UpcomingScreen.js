05845115f1d213cb5d994fb15e2fa42ca8686bb7
OwenRamire <owenomar_ramirez@ucol.mx>
2020-08-06 17:24:20 -0500

    Change screens's backgroundColor

--- conferenceOwen/src/screens/menuUser/UpcomingScreen.js
+++ conferenceOwen/src/screens/menuUser/UpcomingScreen.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import {
   SafeAreaView,
+  StyleSheet,
   View,
   Text,
-  StyleSheet
+  ScrollView,
 } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient';
 import {Colors} from '../../themes/Colors';
-import {upcomingEvents} from '../../events/UpcomingEvents';
+import TodayCard from '../../components/todayCard';
+import NextEvents from '../../components/nextEventsCard';
 
 export default function Upcoming() {
   return (
     <SafeAreaView style={styles.mainContainer}>
-      <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={[Colors.topLeftCorner, Colors.bottomRightCorner]} style={styles.linearGradient}>
-        <View style={styles.cardEvent}>
-          <View style={styles.infoContainer}>
-            <View style={{marginBottom: 15}}>
-              <Text style={[styles.text, styles.todayText]}>Today</Text>
-            </View>
-            <Text style={styles.text}>{upcomingEvents[0].nameEvent}</Text>
-            <Text style={styles.text}>{upcomingEvents[0].speaker}</Text>
-          </View>
-          <View style={styles.dateContainer}>
-            <Text style={[styles.text, styles.hourText]}>{upcomingEvents[0].hour}</Text>
-          </View>
+      <ScrollView>
+        <TodayCard />
+        {/* This view is for helping me with the background color and the responsive design   */}
+        <View style={styles.auxContainer}> 
+          <NextEvents />
         </View>
-      </LinearGradient>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -33,40 +27,13 @@ export default function Upcoming() {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
+    backgroundColor: Colors.backgroundScreen,
   },
-  linearGradient: {
-    flex: 0.3,
-    marginVertical: 30,
-    marginHorizontal: 20,
-  },
-  cardEvent: {
+  auxContainer: {
     flex: 1,
-    flexDirection: 'row',
+    marginHorizontal: 20,
+    marginTop: 30,
+    backgroundColor: Colors.white,
   },
-  infoContainer: {
-    flex: 0.75,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    flexWrap: 'wrap',
-    paddingLeft: 20,
-  },
-  dateContainer: {
-    flex: 0.5,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingBottom: 25,
-    paddingRight: 10
-  },
-  text: {
-    fontSize: 15,
-    fontFamily: 'AlBayan',
-    color: Colors.white,
-    marginBottom: 5
-  },
-  todayText: {
-    fontSize: 18,
-  },
-  hourText: {
-    fontSize: 20,
-  },
+  
 });
